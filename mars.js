3b67d72968c4249a1b89b71e3091c74b572d1994
Richard Burman <111341300+RichBurman@users.noreply.github.com>
2026-04-09 12:15:20 +0100

    Amend typo in comment on MarsGrid

--- mars.js
+++ mars.js
@@ -21,7 +21,7 @@ class MarsGrid {
         // Grid boundaries - the first line of input is the upper-right coordinates of the grid, the lower-left is assumed to be (0, 0).
         this.maxX = upperX;
         this.maxY = upperY;
-        // Robot scent is left at the last grid postion the robot occupied before disappearing over the edge. I will use a set to store the scents.
+        // Robot scent is left at the last grid position the robot occupied before disappearing over the edge. I will use a set to store the scents.
         this.scents = new Set();
         // Whichever way the robot is facing, leftMap and rightMap will determine the new direction the robot will face after a left or right turn.
         this.leftMap = { 'N': 'W', 'W': 'S', 'S': 'E', 'E': 'N' };
